0f4b5e8f2393ed74e506e5b98fc297e4c5166d2f
ilchto <ilkka.torkkel@gmail.com>
2022-08-31 11:52:00 +0300

    u

--- js/tools.js
+++ js/tools.js
@@ -42,3 +42,52 @@ function formatDate(date) {
 function getDaysInMonth(year, month) {
   return new Date(year, month, 0).getDate();
 }
+function isMobile () {
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function dragToScroll (ele) {
+  ele.style.cursor = 'grab';
+
+  let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+  const mouseDownHandler = function (e) {
+    ele.style.cursor = 'grabbing';
+    ele.style.userSelect = 'none';
+
+    pos = {
+      left: ele.scrollLeft,
+      top: ele.scrollTop,
+      // Get the current mouse position
+      x: e.clientX,
+      y: e.clientY,
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+  };
+
+  const mouseMoveHandler = function (e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
+
+    // Scroll the element
+    ele.scrollTop = pos.top - dy;
+    ele.scrollLeft = pos.left - dx;
+  };
+
+  const mouseUpHandler = function () {
+    ele.style.cursor = 'grab';
+    ele.style.removeProperty('user-select');
+
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+  };
+
+  // Attach the handler
+  ele.addEventListener('mousedown', mouseDownHandler);
+}
